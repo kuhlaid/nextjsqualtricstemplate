798f1ba9ed32c4939ce7520a8676f3a6179d1320
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2021-03-23 15:17:32 -0400

    Update list-surveys.js
    Moved the surveys to the env var

--- pages/qualtrics/list-surveys.js
+++ pages/qualtrics/list-surveys.js
@@ -10,31 +10,9 @@ import AccessDenied from "components/access-denied";
 import BtnTakeSurvey from "components/qualtrics/btn-take-survey"
 
 // this is our page output
-// export default function Page({ envVars }) {
-export default function Page({strQurl,strReturnUrl}) {
+export default function Page({strQurl,strReturnUrl, sa}) {
   const [ session, loading ] = useSession()
-  const [ surveyArray, setContent ] = useState({})
-
-  // Fetch our list of surveys
-  useEffect(()=>{
-    const fetchData = async () => {
-      const res = await require("util/app_surveys.json")
-      if (res) { setContent(res) }
-    }
-    fetchData()
-  },[session])
- 
-  // const ph = 'placeholder';
-  // const exportResponses = async () => {
-  //   const res = await fetch('/api/qualtrics/export-responses')
-  //   const json = await res.json()
-  //   if (json.content) { ph = json.content }
-  // }
-
- // $strURL = $this->buildCLUrl(SurveyId, ResponseId, 'HhInterestEdit')."&Q_R=".$objE03Qualtrics->ResponseId;	// we will delete the previous responses locally and on Qualtrics once edits are made
-//  $queryString = '?Q_Language=EN&Interviewer='.__LOGGED_ONYEN__.'&HouseholdNumber='.E03Ds1341::getStatHouseholdNumber($this->strHouseholdId).'&ReturnURL='.urlencode($this->config['app']['e03']['domain'].$this->config['app']['e03']['subdirectory'].'/router.php?UserMode='.E03Session::GetSession(8).'&AppSource='.$this->config['app']['e03']['app_source'].'&routeIn='.$strRouteIn.'&rid=${e://Field/ResponseID}&sid=${e://Field/SurveyID}&HouseholdId='.$this->strHouseholdId.'&Q_R='.$strEditedResponseId);
-//  return ('https://'.$this->config['app']['e03']['qualtrics']['datacenter_id'].'.qualtrics.com/jfe/form/'.$strE03QualtricsId.$queryString);
-   
+  const surveyArray = JSON.parse(sa)
 
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== "undefined" && loading) return null;
@@ -49,16 +27,13 @@ export default function Page({strQurl,strReturnUrl}) {
     return (
       <Layout>
         
-        <h2>ToDo - work on getting responses</h2>
-        <div>http://localhost:3000/qualtrics/[surveyId]/get-response?AppSource=dev&session=[sessionIdFromQualtrics]&startDate=2021/03/15T16:27:59Z&endDate=2021/03/17T16:27:59Z&rid=[responseId]&sid=[surveyId]</div>
-        
         <h1>Qualtrics Survey List</h1>
         <div class="m-3">
-          - Build the list of surveys<br/>
+          - Build the list of surveys from the environment variable<br/>
           - show button to view the responses for a survey</div>
         {/* if we have no surveys from our query */}
         {!surveyArray.IncludeSurveys && <>
-            No surveys
+            No surveys {surveyArray}
           </>}
         {/* if we have surveys in our query */}
         {surveyArray.IncludeSurveys && surveyArray.IncludeSurveys.length>0 && <>
@@ -84,8 +59,8 @@ export async function getServerSideProps() {
 	const r = process.env.QUALTRICS_SERVER_URL
   const s = process.env.QUALTRICS_FORM_PATH
   const t = process.env.NEXTAUTH_URL
-
+  const sa = process.env.SURVEY_OBJ
 	return {
-		props: { strQurl: r+s, strReturnUrl: t }
+		props: { strQurl: r+s, strReturnUrl: t, sa: sa }
 	}
 }
